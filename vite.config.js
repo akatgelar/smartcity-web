af169d03d39b28cd91359bef6d3d0f5e8fb652ac
akatgelar <akatgelar@gmail.com>
2025-08-15 09:40:32 +0700

    feat: vite vue config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import vue from "@vitejs/plugin-vue";
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
     plugins: [
+        vue(),
+        vueJsx(),
+        vueDevTools(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            vue: "vue/dist/vue.esm-bundler.js",
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url))
+        },
+    },
 });
