8b5b10a66fe5da9532a4d5225aa0c7f2b1b324d7
akatgelar <akatgelar@gmail.com>
2025-08-15 09:41:05 +0700

    feat: template & route vue

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::post('/minio/upload', [App\Http\Controllers\MinioController::class, 'upload'])
-    ->withoutMiddleware([Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class]);
+    ->withoutMiddleware([Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class]
+);
+
+Route::get('/{any?}', function () {
+    return view('template');
+// })->where('vue_capture', '[\/\w\.-]*');
+})->where('any', '.*');
