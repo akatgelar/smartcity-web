74f569828843b66ce714cf7346234bae9a3ff905
akatgelar <akatgelar@gmail.com>
2025-08-16 19:18:17 +0700

    fix: route web

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::post('/minio/upload', [App\Http\Controllers\MinioController::class, 'upload'])
-    ->withoutMiddleware([Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class]
-);
-
 Route::get('/{any?}', function () {
     return view('template');
 // })->where('vue_capture', '[\/\w\.-]*');
