0c4b5337638fa411b97cef65d1e6db547df4cb1f
akatgelar <akatgelar@gmail.com>
2025-08-16 19:17:51 +0700

    feat: api test

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\ApiResource;
 
 Route::get("/test-me", function () {
-    return 'Hello from Laravel!';
+    $message = 'Hello from Laravel!';
+    $data = [];
+    $temp = [];
+    $temp['key'] = 'value';
+    array_push($data, $temp);
+
+    $metadata = [];
+    $metadata['page'] = 1;
+    return new ApiResource(200, true, $message, $data, $metadata);
 });
+
+Route::get('/minio/get', [App\Http\Controllers\MinioController::class, 'get'])
+    // ->withoutMiddleware([Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class])
+;
+
+Route::post('/minio/upload', [App\Http\Controllers\MinioController::class, 'upload'])
+    // ->withoutMiddleware([Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class])
+;
