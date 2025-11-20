b59895e6d216750edd575aa8083311f19b0c7a6c
akatgelar <akatgelar@gmail.com>
2025-11-20 16:53:22 +0700

    fix: router web except api

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/{any?}', function () {
     return view('template');
-// })->where('vue_capture', '[\/\w\.-]*');
-})->where('any', '.*');
+})->where('any', '^(?!api).*');
