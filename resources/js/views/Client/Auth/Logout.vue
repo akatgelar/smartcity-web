18478a0ca6cc2e062da9309f47f4111f560b4ba5
akatgelar <akatgelar@gmail.com>
2025-11-21 02:12:49 +0700

    feat: page login

---
+++ resources/js/views/Client/Auth/Logout.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+onMounted(() => {
+    localStorage.clear()
+    router.push('/auth/login');
+})
+
+</script>
